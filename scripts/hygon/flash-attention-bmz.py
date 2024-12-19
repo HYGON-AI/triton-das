bcc0775f5845e64f23a39aa58172fa9390ba59ac
Zhen Xin <zhenxin@hygon.cn>
2024-12-20 05:51:10 +0800

    1> only gqa case use KT when do _attn_bwd_dq for fa bwd. 2> refine fa bwd grid to decrease off calc with div|mod.
    Signed-off-by: Zhen Xin <zhenxin@hygon.cn>

--- scripts/hygon/flash-attention-bmz.py
+++ scripts/hygon/flash-attention-bmz.py
@@ -394,7 +394,8 @@ def _attn_bwd_dq(dq, q, K, KT, V,
                  BLOCK_DMODEL: tl.constexpr,
                  # Filled in by the wrapper.
                  start_m, start_n, num_steps,
-                 MASK: tl.constexpr):
+                 MASK: tl.constexpr,
+                 GQA_FLAG: tl.constexpr):
     offs_m = start_m + tl.arange(0, BLOCK_M2)
     offs_n = start_n + tl.arange(0, BLOCK_N2)
     offs_k = tl.arange(0, BLOCK_DMODEL)
@@ -407,15 +408,16 @@ def _attn_bwd_dq(dq, q, K, KT, V,
         order=(0, 1)
     )
 
-    # Note: bmz add to increase load/store coalesce and increase lds coalesce, not fit for zde
-    K_block_ptr = tl.make_block_ptr(
-        base=KT,
-        shape=(N_CTX, BLOCK_DMODEL),
-        strides=(stride_ttok, stride_td),
-        offsets=(start_n, 0),
-        block_shape=(BLOCK_N2, BLOCK_DMODEL),
-        order=(0,1)
-    )
+    if GQA_FLAG:
+        # Note: bmz add to increase load/store coalesce and increase lds coalesce, not fit for zde
+        K_block_ptr = tl.make_block_ptr(
+            base=KT,
+            shape=(N_CTX, BLOCK_DMODEL),
+            strides=(stride_ttok, stride_td),
+            offsets=(start_n, 0),
+            block_shape=(BLOCK_N2, BLOCK_DMODEL),
+            order=(0,1)
+        )
 
     VT_block_ptr = tl.make_block_ptr(
         base=V,
@@ -442,21 +444,25 @@ def _attn_bwd_dq(dq, q, K, KT, V,
             p = tl.where(mask, p, 0.0)
         # Compute dP and dS.
         vT = tl.load(VT_block_ptr)
-        k = tl.load(K_block_ptr)
         dp = tl.dot(do, vT).to(tl.float32)
 
         ds = p * (dp - Di[:, None])
         ds = ds.to(tl.float16)
         # Compute dQ.
         # NOTE: We need to de-scale dq in the end, because kT was pre-scaled.
-        # dq += tl.dot(ds, tl.trans(kT))
-        dq += tl.dot(ds, k)
+        if GQA_FLAG:
+            k = tl.load(K_block_ptr)
+            dq += tl.dot(ds, k)
+        else:
+            dq += tl.dot(ds, tl.trans(kT))
 
         # Increment pointers.
         curr_n += step_n
         KT_block_ptr = tl.advance(KT_block_ptr, (0, step_n))
         VT_block_ptr = tl.advance(VT_block_ptr, (0, step_n))
-        K_block_ptr = tl.advance(K_block_ptr, (step_n, 0))
+
+        if GQA_FLAG:
+            K_block_ptr = tl.advance(K_block_ptr, (step_n, 0))
     return dq
 
 
@@ -467,7 +473,7 @@ def _attn_bwd_dq(dq, q, K, KT, V,
        triton.Config({'BLOCK_M1': 32, 'BLOCK_N1': 64, 'BLOCK_M2': 64, 'BLOCK_N2': 32, 'BLK_SLICE_FACTOR': 2},
                       num_stages=1, num_warps=4),
    ],
-   key=['H', 'N_CTX', 'BLOCK_DMODEL'],
+   key=['H', 'N_CTX', 'K_HEAD', 'BLOCK_DMODEL'],
 )
 
 @triton.jit
@@ -482,6 +488,7 @@ def _attn_bwd(Q, QT, K, KT, V, sm_scale,
               stride_td, stride_ttok,
               # H = 16, N_CTX = 1024
               H, N_CTX, K_HEAD, q_num_per_group,
+              GQA_FLAG: tl.constexpr,
               BLOCK_DMODEL: tl.constexpr,
               BLOCK_M1: tl.constexpr,
               BLOCK_N1: tl.constexpr,
@@ -489,14 +496,15 @@ def _attn_bwd(Q, QT, K, KT, V, sm_scale,
               BLOCK_N2: tl.constexpr,
               BLK_SLICE_FACTOR: tl.constexpr):
     LN2: tl.constexpr = 0.6931471824645996  # = ln(2)
-    #batch*head 的id
-    bhid = tl.program_id(2)
-    khid = bhid // q_num_per_group
+
     #N_CTX*H*BATCH的偏移，其实就是head的在行上的偏移
     #z与BATCH，H与HEAD是对应的。
+    bid  = tl.program_id(2)
+    hid  = tl.program_id(1)
+    bhid = bid*H + hid
     off_chz = (bhid * N_CTX).to(tl.int64)
-    adj = (stride_h * (bhid % H) + stride_z * (bhid // H)).to(tl.int64)
-    k_adj = (khid // K_HEAD * stride_kz + khid % K_HEAD * stride_h).to(tl.int64)
+    adj = (stride_h * hid + stride_z * bid).to(tl.int64)
+    k_adj = (bid * stride_kz + (hid//q_num_per_group) * stride_h).to(tl.int64)
     pid = tl.program_id(0)
     # offset pointers for batch/head
     # offset pointers for batch/head
@@ -558,7 +566,8 @@ def _attn_bwd(Q, QT, K, KT, V, sm_scale,
                       H, N_CTX,
                       BLOCK_M2, MASK_BLOCK_N2, BLOCK_DMODEL,
                       start_m, end_n - num_steps * MASK_BLOCK_N2, num_steps,
-                      MASK=True
+                      MASK=True,
+                      GQA_FLAG=GQA_FLAG
                       )
     end_n -= num_steps * MASK_BLOCK_N2
     # stage 2
@@ -573,7 +582,8 @@ def _attn_bwd(Q, QT, K, KT, V, sm_scale,
                       H, N_CTX,
                       BLOCK_M2, BLOCK_N2, BLOCK_DMODEL,
                       start_m, end_n - num_steps * BLOCK_N2, num_steps,
-                      MASK=False
+                      MASK=False,
+                      GQA_FLAG=GQA_FLAG
                       )
     # Write back dQ.
     DQ_block_ptr = tl.make_block_ptr(
@@ -779,13 +789,15 @@ class _attention(torch.autograd.Function):
         dk = torch.empty_like(q)
         dv = torch.empty_like(q)
 
-        # Note: bmz add to increase load/store coalesce and increase lds coalesce, not fit for zde
-        qt = q.transpose(-1, -2).contiguous()
-        kt = k.transpose(-1, -2).contiguous()
-
         BATCH, N_HEAD, N_CTX = q.shape[:3]
         K_HEAD = k.shape[1]
         q_num_per_group = N_HEAD // K_HEAD
+
+        # Note: bmz add to increase load/store coalesce and increase lds coalesce, not fit for zde
+        gqa_flag = N_HEAD != K_HEAD
+        qt = q.transpose(-1, -2).contiguous()
+        kt = k.transpose(-1, -2).contiguous() if gqa_flag else k
+
         PRE_BLOCK = 128
         NUM_WARPS, NUM_STAGES = 4, 1
         BLOCK_M1, BLOCK_N1, BLOCK_M2, BLOCK_N2 = 32, 64, 64, 32
@@ -813,8 +825,8 @@ class _attention(torch.autograd.Function):
         #K,V作为外部循环，每个线程块负责一块，Q作为内部循环，每个线程块都要循环遍历一遍。
         grid = lambda META: (
             triton.cdiv(N_CTX, META['BLOCK_N1']),
-            1,
-            BATCH * N_HEAD
+            N_HEAD,
+            BATCH
         )
         _attn_bwd[grid](
             q, qt, arg_k, arg_kt, v, ctx.sm_scale, do, dq, dk, dv,
@@ -822,6 +834,7 @@ class _attention(torch.autograd.Function):
             q.stride(0), q.stride(1), q.stride(2), q.stride(3),k.stride(0),
             qt.stride(2), qt.stride(3),
             N_HEAD, N_CTX, K_HEAD, q_num_per_group,
+            GQA_FLAG=gqa_flag,
             BLOCK_DMODEL=ctx.BLOCK_DMODEL
         )
 
