a276cc01fee0e660e68d57f215176c52fc32ef80
shenruihan <shenruihan@hygon.cn>
2025-01-10 17:40:07 +0800

    fix limit about min_dot_size

--- python/test/unit/language/test_compile_errors.py
+++ python/test/unit/language/test_compile_errors.py
@@ -378,6 +378,42 @@ def test_fp8_support(dtype):
             raise assertion_err from e.value
 
 
+@pytest.mark.parametrize("dtype", [tl.int8, tl.bfloat16, tl.float16, tl.float32])
+def test_min_dot_size(dtype):
+    error_msg = "Input shapes should have "
+    if is_cuda():
+        if dtype.primitive_bitwidth == 8:
+            error_msg += "M >= 16, N >= 16 and K >= 32"
+        else:
+            error_msg = "M >= 16, N >= 16 and K >= 16"
+    elif is_hip():
+        if dtype == tl.int8:
+            error_msg += "M >= 16, N >= 16 and K >= 32"
+        elif dtype == tl.float16 or dtype == tl.bfloat16:
+            error_msg += "M >= 16, N >= 16 and K >= 16"
+        else:
+            error_msg = "M >= 16, N >= 16 and K >= 8"
+    else:
+        pytest.skip("Test only supported on CUDA and HIP")
+
+    @triton.jit
+    def dot_kernel(dtype: tl.constexpr):
+        SIZE_M: tl.constexpr = 8
+        SIZE_N: tl.constexpr = 8
+        SIZE_K: tl.constexpr = 8
+        a = tl.full((SIZE_M, SIZE_K), 0.0, dtype)
+        b = tl.full((SIZE_K, SIZE_N), 0.0, dtype)
+        tl.dot(a, b)
+
+    with pytest.raises(CompilationError) as e:
+        triton.compile(
+            triton.compiler.ASTSource(fn=dot_kernel, signature={}, constants={"dtype": dtype}))
+    try:
+        assert (error_msg in str(e.value.__cause__))
+    except AssertionError as assertion_err:
+        raise assertion_err from e.value
+
+
 def test_max_num_imprecise_acc_limit():
 
     @triton.jit
--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -14,15 +14,14 @@ from pathlib import Path
 
 def min_dot_size(target: GPUTarget):
     arch_str = target.arch
-    # CDNA 3.0 supports k==8 in all mfma variants except for int8
-    # (where the smallest `k` supported is 16)
-    if "gfx94" in arch_str:
-        return lambda lhsType, rhsType: (16, 16, 16) if (lhsType.is_int8() or rhsType.is_int8()) else (16, 16, 8)
-    # CDNA 2.0 always supports `k==8`
-    if "gfx9" in arch_str:
-        return lambda lhsType, rhsType: (16, 16, 8)
-    # Other architectures will only support 16,16,16
-    return lambda lhsType, rhsType: (16, 16, 16)
+    assert arch_str == "gfx928" or arch_str == "gfx936" or arch_str == "gfx938", f"Unsupported gfx version"
+    
+    return lambda lhsType, rhsType: (
+        #(16, 16, 64) if (lhsType.scalar.is_int4() or rhsType.scalar.is_int4()) else     #only gfx938 support, but triton not support now
+        (16, 16, 32) if (lhsType.scalar.is_int8() or rhsType.scalar.is_int8()) else 
+        (16, 16, 16) if (lhsType.scalar.is_fp16() or rhsType.scalar.is_fp16() or lhsType.scalar.is_bf16() or rhsType.scalar.is_bf16()) else 
+        (16, 16, 8) #fp32
+    )
 
 
 @dataclass(frozen=True)
