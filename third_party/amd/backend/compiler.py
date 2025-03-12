d6cc7bac9c7ca3dd97838ef1593278e5caedd1de
Hui BU <buhui@hygon.cn>
2025-03-12 11:16:56 +0000

    [HYGON] Use buffer ops by default.

--- third_party/amd/backend/compiler.py
+++ third_party/amd/backend/compiler.py
@@ -257,7 +257,8 @@ class HIPBackend(BaseBackend):
         passes.ttgpuir.add_optimize_dot_operands(pm, True)
 
         stream_prefetch = os.getenv("TRITON_HIP_STREAM_PREFETCH", "0") == "1"
-        use_buffer_ops = os.environ.get("AMDGCN_USE_BUFFER_OPS", "0") == "1"
+        # In most cases, buffer ops are beneficial for performance on HCUs.
+        use_buffer_ops = os.environ.get("AMDGCN_USE_BUFFER_OPS", "1") == "1"
 
         # The `local-prefetch` scheduling variant requires turning on buffer ops.
         if options.instruction_sched_variant == "local-prefetch":
