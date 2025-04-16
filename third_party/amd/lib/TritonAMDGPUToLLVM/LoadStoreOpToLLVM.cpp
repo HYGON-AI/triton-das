865807159193871870157c1f35af3b1da5dfe663
lijianhua <lijianhua@hygon.cn>
2025-04-16 17:35:48 +0800

    Revert "[HYGON] atomicrmw fadd adaptation" (#DCUSW-2747)
    This reverts commit 99d99c836008f3345816389d7a7f4c3204cce4c2.

--- third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
+++ third_party/amd/lib/TritonAMDGPUToLLVM/LoadStoreOpToLLVM.cpp
@@ -715,13 +715,9 @@ struct AtomicCASOpConversion
 };
 
 bool supportsGlobalAtomicF16PackedAndDpp(triton::AMD::ISAFamily isaFamily) {
-  // return isaFamily == triton::AMD::ISAFamily::CDNA1 ||
-  //        isaFamily == triton::AMD::ISAFamily::CDNA2 ||
-  //        isaFamily == triton::AMD::ISAFamily::CDNA3;
-  // HYGON TODO: adaptation of vector type for atomicrmw.
-  // use this optimize will introduce vector type for atomicrmw, but atomicrmw
-  // don't support vector type in backend now, so return false directly.
-  return false;
+  return isaFamily == triton::AMD::ISAFamily::CDNA1 ||
+         isaFamily == triton::AMD::ISAFamily::CDNA2 ||
+         isaFamily == triton::AMD::ISAFamily::CDNA3;
 }
 
 Value generateI32DppMove(PatternRewriter &rewriter, Value val, int dppCtrl) {
