c5279a5a9c83de22682127a5f0abdf9a26b52f63
Shuang Huang <huangshuang@hygon.cn>
2025-05-29 07:34:56 +0000

    [hcutuner] Support cache best config to avoid repetitive tuning

--- python/test/unit/utils/test_hcutuner.py
+++ python/test/unit/utils/test_hcutuner.py
@@ -8,6 +8,7 @@ import triton.language as tl
 import pytest
 from triton.runtime.cache import default_cache_dir
 import subprocess
+from triton.utils.hcutuner import get_config_key
 
 
 def do_bench(kernel_call, quantiles):
@@ -15,7 +16,8 @@ def do_bench(kernel_call, quantiles):
 
 
 def get_save_config_root_dir():
-    return f"{default_cache_dir()}/configs"
+    cache_dir = os.getenv("TRITON_CACHE_DIR", "").strip() or default_cache_dir()
+    return f"{cache_dir}/configs"
 
 
 def get_gpu_label():
@@ -35,6 +37,17 @@ def get_save_config_files(fname, device):
     sub_dir = os.path.join(root_dir, fname, get_device_name(device))
     res = []
     for dirpath, _, filenames in os.walk(sub_dir):
+        for filename in filenames:
+            if filename != "config.json":
+                continue
+            res.append(os.path.join(dirpath, filename))
+    return res
+
+
+def get_config_cache_files(fn, *args, **kwargs):
+    res = []
+    cache_manager = fn.get_config_cache_manager(*args, **kwargs)
+    for dirpath, _, filenames in os.walk(cache_manager.key_dir):
         for filename in filenames:
             if filename != "config.json":
                 continue
@@ -222,3 +235,160 @@ def test_mp_restore_config(device, world_size):
     config = triton.utils.get_optimal_config("_kernel_mp", src, N)
     _kernel_mp[grid](src, N, **config)
     triton.testing.assert_close(src, torch.ones_like(src))
+
+
+@pytest.mark.parametrize('device', ['cuda'])
+@pytest.mark.parametrize('always_tuning', [True, False])
+def test_restore_cache(device: str, always_tuning: bool):
+    configs = [triton.Config(kwargs={'BLOCK_SIZE': 32}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+    @triton.utils.hcutune(configs=configs, key=['M', 'N'], restore_value=['src'], do_bench=do_bench,
+                          always_tuning=always_tuning)
+    @triton.jit
+    def _kernel_cache(src, M, N, BLOCK_SIZE: tl.constexpr):
+        offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        N = N * M // M # dummy M
+        x = tl.load(src + offsets, mask=offsets < N) + 1
+        tl.store(src + offsets, x, mask=offsets < N)
+
+    fn = _kernel_cache
+    SIZE = 4
+    M = [i * 128 for i in range(1, SIZE + 1)]
+    N = [i * 128 for i in range(1, SIZE + 1)]
+    src = [torch.zeros(n, device=device) for n in N]
+
+    for _src, m, n in zip(src, M, N):
+        grid = lambda META: (triton.cdiv(n, META['BLOCK_SIZE']), )
+        fn[grid](_src, m, n)
+        triton.testing.assert_close(_src, torch.ones_like(_src))
+
+    files = get_config_cache_files(fn, src[0], M[0], N[0])
+    assert len(files) == 1
+    fn.restore_tuned_cache(src[0], M[0], N[0])
+    assert len(fn.cache) == SIZE
+
+    # cache hit
+    keys = [str(o) for o in fn.cache.keys()]
+    for _src, m, n in zip(src, M, N):
+        key = get_config_key(fn.arg_names, fn.keys, _src, m, n)
+        assert key in keys
+
+
+@pytest.mark.parametrize('device', ['cuda'])
+def test_cache_key_changes(device: str):
+    M, N = 128, 128
+    src = torch.zeros(N, device=device)
+
+    # change configs: {'BLOCK_SIZE': 32} -> {'BLOCK_SIZE': 64}
+    configs = [triton.Config(kwargs={'BLOCK_SIZE': 64}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+    @triton.utils.hcutune(configs=configs, key=['M', 'N'], restore_value=['src'], do_bench=do_bench)
+    @triton.jit
+    def _kernel_cache(src, M, N, BLOCK_SIZE: tl.constexpr):
+        offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        N = N * M // M # dummy M
+        x = tl.load(src + offsets, mask=offsets < N) + 1
+        tl.store(src + offsets, x, mask=offsets < N)
+
+    # Not found the expected config cache of _kernel_cache
+    assert len(get_config_cache_files(_kernel_cache, src, M, N)) == 0
+
+    # change autotune params: use_cuda_graph=True
+    configs = [triton.Config(kwargs={'BLOCK_SIZE': 32}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+    @triton.utils.hcutune(configs=configs, key=['M', 'N'], restore_value=['src'], do_bench=do_bench,
+                          use_cuda_graph=True)
+    @triton.jit
+    def _kernel_cache(src, M, N, BLOCK_SIZE: tl.constexpr):
+        offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        N = N * M // M # dummy M
+        x = tl.load(src + offsets, mask=offsets < N) + 1
+        tl.store(src + offsets, x, mask=offsets < N)
+
+    assert len(get_config_cache_files(_kernel_cache, src, M, N)) == 0
+
+    # change autotune key: ['M', 'N'] -> ['N', 'M']
+    configs = [triton.Config(kwargs={'BLOCK_SIZE': 32}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+    @triton.utils.hcutune(configs=configs, key=['N', 'M'], restore_value=['src'], do_bench=do_bench)
+    @triton.jit
+    def _kernel_cache(src, M, N, BLOCK_SIZE: tl.constexpr):
+        offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        N = N * M // M # dummy M
+        x = tl.load(src + offsets, mask=offsets < N) + 1
+        tl.store(src + offsets, x, mask=offsets < N)
+
+    assert len(get_config_cache_files(_kernel_cache, src, M, N)) == 0
+
+    # change code
+    configs = [triton.Config(kwargs={'BLOCK_SIZE': 32}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+    @triton.utils.hcutune(configs=configs, key=['M', 'N'], restore_value=['src'], do_bench=do_bench)
+    @triton.jit
+    def _kernel_cache(src, M, N, BLOCK_SIZE: tl.constexpr):
+        offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        x = tl.load(src + offsets, mask=offsets < N) + 1
+        tl.store(src + offsets, x, mask=offsets < N)
+
+    assert len(get_config_cache_files(_kernel_cache, src, M, N)) == 0
+
+
+@pytest.mark.parametrize('device', ['cuda'])
+@pytest.mark.parametrize('world_size', [4])
+def test_mp_cache_config(device: str, world_size):
+    code = """
+import os
+import sys
+import torch
+import triton
+import triton.language as tl
+
+def do_bench(kernel_call, quantiles):
+    return triton.testing.do_bench(kernel_call, quantiles=quantiles, warmup=1, rep=1)
+
+N = int(sys.argv[1])
+device = sys.argv[2]
+
+configs = [triton.Config(kwargs={'BLOCK_SIZE': 32}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+
+@triton.utils.hcutune(configs=configs, key=['N'], restore_value=['src'], do_bench=do_bench)
+@triton.jit
+def _kernel_mp_cache(src, N, BLOCK_SIZE: tl.constexpr):
+    offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+    x = tl.load(src + offsets, mask=offsets < N) + 1
+    tl.store(src + offsets, x, mask=offsets < N)
+
+def main():
+    src = torch.zeros(N, device=device)
+    grid = lambda META: (triton.cdiv(N, META['BLOCK_SIZE']), )
+    _kernel_mp_cache[grid](src, N)
+    triton.testing.assert_close(src, torch.ones_like(src))
+
+if __name__ == "__main__":
+    main()
+    """
+
+    # multi-process tuning
+    temp_filename = None
+    with tempfile.NamedTemporaryFile(mode='w', suffix='.py', delete=False) as f:
+        f.write(code.strip())
+        temp_filename = f.name
+
+    N = [i * 64 for i in range(1, world_size + 1)]
+    src = [torch.zeros(n, device=device) for n in N]
+    processes = []
+    for i in range(world_size):
+        cmd = ["python3", temp_filename, str(N[i]), device]
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        processes.append(p)
+
+    for p in processes:
+        p.wait()
+        assert p.returncode == 0
+
+    configs = [triton.Config(kwargs={'BLOCK_SIZE': 32}), triton.Config(kwargs={'BLOCK_SIZE': 128})]
+
+    @triton.utils.hcutune(configs=configs, key=['N'], restore_value=['src'], do_bench=do_bench)
+    @triton.jit
+    def _kernel_mp_cache(src, N, BLOCK_SIZE: tl.constexpr):
+        offsets = tl.program_id(0) * BLOCK_SIZE + tl.arange(0, BLOCK_SIZE)
+        x = tl.load(src + offsets, mask=offsets < N) + 1
+        tl.store(src + offsets, x, mask=offsets < N)
+
+    files = get_config_cache_files(_kernel_mp_cache, src[0], N[0])
+    assert len(files) == world_size
--- python/triton/utils/hcutuner.py
+++ python/triton/utils/hcutuner.py
@@ -4,11 +4,24 @@ import os
 import uuid
 import json
 import torch
+import uuid
+import hashlib
 import triton
 from collections import defaultdict
-from triton.runtime.cache import default_cache_dir
+from triton.runtime.cache import default_cache_dir, default_dump_dir, default_override_dir
+
+from typing import Dict
+from distutils.util import strtobool
+
+from triton.runtime.cache import FileCacheManager, _base32
+from triton.compiler.compiler import make_backend, triton_key, ASTSource
+from triton.runtime.driver import driver
+from triton._C.libtriton import get_cache_invalidating_env_vars
+from triton.runtime.jit import mangle_type, DependenciesFinder
 
 file_cache = {}
+manager_cache = {}
+config_cache = {}
 
 
 def _get_result_template(arg_names: list, keys:list):
@@ -21,43 +34,112 @@ def _get_result_template(arg_names: list, keys:list):
 
 
 def get_config_cache_dir():
-    return os.path.join(default_cache_dir(), "configs")
+    cache_dir = os.getenv("TRITON_CACHE_DIR", "").strip() or default_cache_dir()
+    return os.path.join(cache_dir, "configs")
 
 
 class Hcutuner(triton.runtime.Autotuner):
     """
     Re-implements Triton autotune to support custom operations:
-    1. save best config to files(under ~/.triton/json).
+    1. save best config to files(under ~/.triton/cache).
+    2. cache and restore best config to avoid repetitive tuning
+    3. support multi-process tuning
     """
+    def __init__(self, fn, arg_names, configs, key, reset_to_zero, restore_value, pre_hook=None,
+                  post_hook=None, prune_configs_by: Dict = None, warmup=None, rep=None,
+                  use_cuda_graph=False, do_bench=None, perf_debug=False, perf_profiling=False,
+                  always_tuning=False):
+        super().__init__(fn, arg_names, configs, key, reset_to_zero, restore_value, pre_hook=pre_hook,
+                         post_hook=post_hook, prune_configs_by=prune_configs_by, warmup=warmup, rep=rep,
+                         use_cuda_graph=use_cuda_graph, do_bench=do_bench, perf_debug=perf_debug,
+                         perf_profiling=perf_profiling)
+
+        # create dir for saving config (e.g. ~/.triton/cache/configs/...)
+        device_name = get_gpu_label()
+        run_id = str(uuid.uuid4()) # random run_id for multiprocessing
+        self.save_config_dir = os.path.join(get_config_cache_dir(),
+                                            fn.__name__,
+                                            device_name,
+                                            run_id)
+        os.makedirs(self.save_config_dir, exist_ok=True)
+
+        # Not use the funed configs from cache to save time
+        self.flag_restore_tuned_cache = not always_tuning
+
+        self.autotune_param_hash = self._get_param_hash()
+        self.autotune_key_hash = get_list_hash(self.keys)
+        self.kernel_config_hash = get_config_list_hash(self.configs)
 
     def run(self, *args, **kwargs):
+        if self.flag_restore_tuned_cache:
+            self.restore_tuned_cache(*args, **kwargs)
+            self.flag_restore_tuned_cache = False
+
         ret = super().run(*args, **kwargs)
-        self.save_config(self.best_config, *args, **kwargs)
+        self.save_config(*args, **kwargs)
+        self.cache_config(*args, **kwargs)
         return ret
 
-    def save_config(self, config, *args, **kwargs):
-        if not hasattr(self, 'save_config_dir') or self.save_config_dir is None:
-            device_name = get_gpu_label()
-            # random run_id for multiprocessing
-            run_id = str(uuid.uuid4())
-            self.save_config_dir = os.path.join(get_config_cache_dir(),
-                                                self.fn.__name__,
-                                                device_name,
-                                                run_id)
-            os.makedirs(self.save_config_dir, exist_ok=True)
+    def save_config(self, *args, **kwargs):
+        """
+        Save best config as JSON file to cache dir: triton_cache_dir/configs/...
+        """
         fname = os.path.join(self.save_config_dir, "config.json")
         key = get_config_key(self.arg_names, self.keys, *args, **kwargs)
         configs = file_cache[fname] if fname in file_cache else \
                                     _get_result_template(self.arg_names, self.keys)
         if key not in configs['configs']:
-            configs['configs'][key] = config.all_kwargs()
+            configs['configs'][key] = self.best_config.all_kwargs()
             with open(fname, "w") as f:
                 json.dump(configs, f, indent=4)
             file_cache[fname] = configs
 
+    def cache_config(self, *args, **kwargs):
+        """
+        Cache best config by ConfigCacheManager to avoid repetitive tuning
+        """
+        if not self.best_config:
+            return
+        keys = self.keys
+        arg_names = self.arg_names
+        cache_manager = self.get_config_cache_manager(*args, **kwargs)
+        key = cache_manager.key
+        cache_json = config_cache[key] if key in config_cache else _get_result_template(arg_names, keys)
+        config_key = get_config_key(arg_names, keys, *args, **kwargs)
+        if config_key not in cache_json:
+            cache_json['configs'][config_key] = self.best_config.all_kwargs()
+            # print(f"[hcutuner] added best config to {cache_manager.cache_dir}")
+            cache_manager.put(cache_json, "config.json", False)
+            config_cache[key] = cache_json
+
+    def restore_tuned_cache(self, *args, **kwargs):
+        data = None
+        cache_manager = self.get_config_cache_manager(*args, **kwargs)
+        data = cache_manager.get("config.json")
+        if data and self.keys == data['keys']:
+            for k, v in data['configs'].items():
+                kt = _create_tuple(k)
+                kv = _create_config_args(v)
+                self.cache[kt] = triton.Config(**kv)
+
+    def get_config_cache_manager(self, *args, **kwargs):
+        key = _base32(_get_cache_hash(self.fn, self.autotune_param_hash, self.autotune_key_hash,
+                                      self.kernel_config_hash, *args, **kwargs))
+        if key not in manager_cache:
+            manager_cache[key] = ConfigCacheManager(key)
+        return manager_cache[key]
+
+    def _get_param_hash(self):
+        s = f"autotuner params: warmup {self.num_warmups} rep {self.num_reps} use_cuda_graphs {self.use_cuda_graph}"
+        # TODO: how to hash the custom hooks?
+        #  possible would be str(inspect.Signature().from_callable(self.pre_hook))
+        #  maybe not relevant since should not influence the autotuner result
+        return get_string_hash(s)
+
 
 def hcutune(configs, key, prune_configs_by=None, reset_to_zero=None, restore_value=None, pre_hook=None, post_hook=None,
-             warmup=None, rep=None, use_cuda_graph=False, do_bench=None, perf_debug=False, perf_profiling=False):
+             warmup=None, rep=None, use_cuda_graph=False, do_bench=None, perf_debug=False, perf_profiling=False,
+             always_tuning=False):
     """
     Decorator for auto-tuning a :code:`triton.jit`'d function. Its usage is consistent with Triton autotune.
     It's save best config file to `$TRITON_HOME_DIR/.triton/json` directory.
@@ -113,12 +195,15 @@ def hcutune(configs, key, prune_configs_by=None, reset_to_zero=None, restore_val
     :type rep: int
     :param do_bench: a benchmark function to measure the time of each run.
     :type do_bench: lambda fn, quantiles
+    :param always_tuning: Do not use the tuned cache; always perform auto-tuning.
+    :type always_tuning: bool
     """
 
     def decorator(fn):
         return Hcutuner(fn, fn.arg_names, configs, key, reset_to_zero, restore_value, pre_hook=pre_hook,
                          post_hook=post_hook, prune_configs_by=prune_configs_by, warmup=warmup, rep=rep,
-                         use_cuda_graph=use_cuda_graph, perf_debug=perf_debug, perf_profiling=perf_profiling)
+                         use_cuda_graph=use_cuda_graph, perf_debug=perf_debug, perf_profiling=perf_profiling,
+                         always_tuning=always_tuning)
 
     return decorator
 
@@ -171,16 +256,12 @@ class ConfigLoader:
     def load_all(self):
         def parse_all_config_files(root_dir):
             res = []
-            for dirpath, _, filenames in os.walk(root_dir):
-                for filename in filenames:
-                    if filename != "config.json":
-                        continue
-                    fullpath = os.path.join(dirpath, filename)
-                    relative_path = os.path.relpath(fullpath, root_dir)
-                    parts = relative_path.split("/")
-                    assert len(parts) == 4
-                    # (filepath, op_name, device_name, run_id)
-                    res.append((fullpath, *parts[:3]))
+            for fpath in get_config_files(root_dir):
+                relative_path = os.path.relpath(fpath, root_dir)
+                parts = relative_path.split("/")
+                assert len(parts) == 4
+                # (filepath, op_name, device_name, run_id)
+                res.append((fpath, *parts[:3]))
             return res
 
         for fpath, op, device, _ in parse_all_config_files(self.config_dir):
@@ -196,3 +277,207 @@ class ConfigLoader:
 
     def get_tuned_cache(self, op_name):
         return self.tuned_cache[op_name] if op_name in self.tuned_cache else None
+
+
+class ConfigCacheManager(FileCacheManager):
+    """
+    Re-implements Triton's cache manager to:
+    1. put/get JSON format file(s)
+    2. allow a separate sub-directory for each process
+    """
+    def __init__(self, key, override=False, dump=False):
+        self.key = key
+        self.lock_path = None
+        if dump:
+            self.cache_dir = os.getenv("TRITON_DUMP_DIR", "").strip() or default_dump_dir()
+            self.cache_dir = os.path.join(self.cache_dir, self.key)
+            self.lock_path = os.path.join(self.cache_dir, "lock")
+            os.makedirs(self.cache_dir, exist_ok=True)
+        elif override:
+            self.cache_dir = os.getenv("TRITON_OVERRIDE_DIR", "").strip() or default_override_dir()
+            self.cache_dir = os.path.join(self.cache_dir, self.key)
+        else:
+            # create cache directory if it doesn't exist
+            self.cache_dir = os.getenv("TRITON_CACHE_DIR", "").strip() or default_cache_dir()
+            if self.cache_dir:
+                # random run_id for multiprocessing
+                run_id = str(uuid.uuid4())
+                self.key_dir = os.path.join(self.cache_dir, self.key)
+                self.cache_dir = os.path.join(self.key_dir, run_id)
+                self.lock_path = os.path.join(self.cache_dir, "lock")
+                os.makedirs(self.cache_dir, exist_ok=True)
+            else:
+                raise RuntimeError("Could not create or locate cache dir")
+
+    def get(self, filename):
+        data = []
+        ret = None
+        for fpath in get_config_files(self.key_dir, filename):
+            try:
+                with open(fpath) as f:
+                    data.append(json.load(f))
+            except Exception as e:
+                raise Exception(f"[hcutuner] Fail to load best config {fpath} : {e}")
+        if data:
+            ret = _get_result_template(data[0]['arg_names'], data[0]['keys'])
+            ret['configs'] = {k: v for d in data for k, v in d['configs'].items()}
+        return ret
+
+    def put(self, data, filename, binary=True) -> str:
+        if not self.cache_dir:
+            raise RuntimeError("Could not create or locate cache dir")
+        assert self.lock_path is not None
+        filepath = self._make_path(filename)
+        # Random ID to avoid any collisions
+        rnd_id = str(uuid.uuid4())
+        # we use the PID in case a bunch of these around so we can see what PID made it
+        pid = os.getpid()
+        # use temp dir to be robust against program interruptions
+        temp_dir = os.path.join(self.cache_dir, f"tmp.pid_{pid}_{rnd_id}")
+        os.makedirs(temp_dir, exist_ok=True)
+        temp_path = os.path.join(temp_dir, filename)
+
+        with open(temp_path, "w") as f:
+            json.dump(data, f)
+        # Replace is guaranteed to be atomic on POSIX systems if it succeeds
+        # so filepath cannot see a partial write
+        os.replace(temp_path, filepath)
+        os.removedirs(temp_dir)
+        return filepath
+
+
+def _get_src_hash(src: ASTSource):
+    """
+    Non-key constants are considered not feature for generating kernel.
+    So, they are not contain in hash.
+
+    Adapted from: ASTSource.hash()
+    """
+    def get_fn_hash(fn):
+        dependencies_finder = DependenciesFinder(name=fn.__name__, globals={}, src=fn.src)
+        dependencies_finder.visit(fn.parse())
+        return dependencies_finder.ret
+
+    sorted_sig = [v for k, v in sorted(src.signature.items())]
+    key = f"{get_fn_hash(src.fn)}-{src.attrs.hash()}-{sorted_sig}"
+    return key
+
+
+def _get_cache_hash(fn, autotune_param_hash, key_hash, kernel_config_hash, *args, **kwargs):
+    """
+    Create a hash for locating the best config cache in the triton cache
+    directory(~/.triton/cache by default). Where there is a config.json
+    containing all the tuned best configs for a specified jit function.
+
+    hash format: triton_code-backend-option-env-src-autotune_params-tune_keys-kernel_configs
+    Adapted from: triton/compiler/compiler.py:compile()
+    """
+    target = driver.active.get_current_target()
+    backend = make_backend(target)
+
+    if fn.binder is None:
+        fn.create_binder(backend)
+
+    nargs = dict(zip(fn.arg_names, args))
+    all_args = {**nargs, **kwargs}
+    sigkeys = [fn.params[i].name for i in fn.non_constexpr_indices]
+    signature = {k: mangle_type(all_args[k]) for k in sigkeys}
+    options = backend.parse_options(kwargs).__dict__
+
+    constants = {}
+    for i in fn.constexpr_indices:
+        if (name := fn.arg_names[i]) in all_args:
+            constants[name] = all_args[name]
+
+    src = fn.ASTSource(fn, signature, constants)
+    extra_options = src.parse_options()
+    options = backend.parse_options(dict(options or dict(), **extra_options))
+
+    env_vars = get_cache_invalidating_env_vars()
+    key = f"{triton_key()}-{backend.hash()}-{options.hash()}-{str(sorted(env_vars.items()))}"
+    key += f"-{_get_src_hash(src)}-{autotune_param_hash}-{key_hash}-{kernel_config_hash}"
+    return get_string_hash(key)
+
+
+def _create_tuple(k):
+    s = k[1:-1]
+    entries = s.split(", ")
+    ret = []
+    for e in entries:
+        if e[0] == "'" or e[0] == '"':
+            ret.append(e[1:-1])
+        else:
+            ret.append(eval(e))
+    ret_t = tuple(ret)
+    return ret_t
+
+
+__int_config_args__ = ["num_warps", "num_stages", "num_ctas"]
+__int_or_none_config_args__ = ["maxnreg"]
+__bool_config_args__ = ["enable_warp_specialization"]
+__config_args__ = (
+    ["pre_hook"]
+    + __int_config_args__
+    + __bool_config_args__
+    + __int_or_none_config_args__
+)
+__skip_config_args__ = ["enable_persistent"]
+
+
+def _create_config_args(args):
+    ret = {"kwargs": {}}
+    for k, v in args.items():
+        if k in __skip_config_args__:
+            continue
+        if k in __config_args__:
+            if k in __int_config_args__:
+                ret[k] = int(v)
+            elif k in __bool_config_args__:
+                ret[k] = bool(strtobool(v))
+            elif k in __int_or_none_config_args__:
+                try:
+                    ret[k] = int(v)
+                except ValueError:
+                    ret[k] = None
+            else:
+                ret[k] = v
+        else:
+            try:
+                ret["kwargs"][k] = int(v)
+            except ValueError:
+                try:
+                    ret["kwargs"][k] = bool(strtobool(v))
+                except ValueError:
+                    print(
+                        f"[hcutuner] WARNING: can't determine type of kwarg {k}: {v}"
+                    )
+                    ret["kwargs"][k] = v
+    return ret
+
+
+def get_config_files(root_dir, filename="config.json"):
+    """ Sort by file creation time in ascending order """
+    files = []
+    for dirpath, _, filenames in os.walk(root_dir):
+        for fname in filenames:
+            if fname != filename:
+                continue
+            files.append(os.path.join(dirpath, filename))
+    files.sort(key=lambda f: os.path.getctime(f))
+    return files
+
+
+def get_config_list_hash(configs):
+    s = "|"
+    for c in configs:
+        s += f"{c}|"
+    return hashlib.sha256(s.encode("utf-8")).hexdigest()
+
+
+def get_list_hash(l):
+    s = "|".join(l)
+    return hashlib.sha256(s.encode("utf-8")).hexdigest()
+
+
+def get_string_hash(s):
+    return hashlib.sha256(s.encode("utf-8")).hexdigest()
