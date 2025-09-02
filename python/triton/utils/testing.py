28df57c632dcdfe5e703d5f663d99592d93292d2
Shuang Huang <huangshuang@hygon.cn>
2025-09-02 10:28:04 +0000

    [hcutuner] Bug fix of benchmark run

--- python/triton/utils/testing.py
+++ python/triton/utils/testing.py
@@ -8,6 +8,7 @@ class MPMark(Mark):
         if save_path:
             run_id = str(uuid.uuid4()) # random run_id for multiprocessing
             save_path = os.path.join(save_path, run_id)
+            os.makedirs(save_path, exist_ok=True)
         return super().run(show_plots=show_plots, print_data=print_data, save_path=save_path,
                            return_df=return_df, **kwargs)
 
