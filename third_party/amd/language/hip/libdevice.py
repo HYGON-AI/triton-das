c7a79d91cf17e5b6e888e6c82a2a48ec1c6b104d
Zhen Xin <zhenxin@hygon.cn>
2026-04-03 06:21:02 +0000

    fix a missing round op issue.
    Signed-off-by: Zhen Xin <zhenxin@hygon.cn>

--- third_party/amd/language/hip/libdevice.py
+++ third_party/amd/language/hip/libdevice.py
@@ -482,6 +482,15 @@ def ilogb(arg0, _semantic=None):
 
 
 @core.extern
+def round(arg0, _semantic=None):
+    return core.extern_elementwise(
+        "", "", [arg0], {
+            (core.dtype("fp32"), ): ("__ocml_round_f32", core.dtype("fp32")),
+            (core.dtype("fp64"), ): ("__ocml_round_f64", core.dtype("fp64")),
+        }, is_pure=True, _semantic=_semantic)
+
+
+@core.extern
 def __triton_hip_fdividef(arg0, arg1, _semantic=None):
     return core.extern_elementwise(
         "", "", [arg0, arg1], {
